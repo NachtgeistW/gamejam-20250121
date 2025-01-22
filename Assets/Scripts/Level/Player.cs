d809c4c27afb4c3019a8b25057fac141404803d8
夜轮_NachtgeistW <nachtgeistw@protonmail.com>
2025-01-22 14:52:28 +0800

    Code cleanup

--- Assets/Scripts/Level/Player.cs
+++ Assets/Scripts/Level/Player.cs
@@ -8,12 +8,12 @@ namespace Level
     {
         [field: SerializeField] public float Speed { get; private set; }
         [field: SerializeField] public Vector2 Position { get; private set; }
-        
+
         [field: SerializeField] public float InputX { get; private set; }
         [field: SerializeField] public float InputY { get; private set; }
         private Vector2 movementInput;
         private bool isMoving;
-        
+
         //[field: SerializeField] public Sprite Sprite { get; private set; }
 
         private void Update()
@@ -29,8 +29,9 @@ namespace Level
             InputY = Input.GetAxisRaw("Vertical");
             Vector2 movement = new Vector2(InputX, InputY);
             transform.LookAt((Vector2)transform.position + movement);
-            
-            transform.position +=new Vector3 ((movement * Speed * Time.deltaTime).x, (movement * Speed * Time.deltaTime).y,0f);
+
+            transform.position += new Vector3((movement * (Speed * Time.deltaTime)).x,
+                (movement * (Speed * Time.deltaTime)).y, 0f);
             //throw new System.NotImplementedException();
         }
 
