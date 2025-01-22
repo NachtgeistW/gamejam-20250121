d5ee93ecd967132c3055389fd63edac6b8503fc9
NachtgeistW <nachtgeistw@outlook.com>
2025-01-23 01:57:24 +0800

    解决墙一开始会显示的问题

--- Assets/Scripts/Level/RadioWave.cs
+++ Assets/Scripts/Level/RadioWave.cs
@@ -37,23 +37,14 @@ namespace Level
         void Update()
         {
 
-            if (!Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space))
             {
                 //旋转扫描效果
-                transform.Rotate(0, 0, rotatePerSecond * Time.deltaTime);
+                //transform.Rotate(0, 0, rotatePerSecond * Time.deltaTime);
 
                 // 在扇形区域内检测墙壁
                 CheckWallsInSector();
             }
-
-            for (var x = wallTilemap.cellBounds.xMin; x < wallTilemap.cellBounds.xMax; x++)
-            {
-                for (var y = wallTilemap.cellBounds.yMin; y < wallTilemap.cellBounds.yMax; y++)
-                {
-                    wallTilemap.SetColor(new Vector3Int(x, y, 0), new Color(1, 1, 1, 0));
-                }
-            }
-
         }
 
         void CheckWallsInSector()
@@ -103,7 +94,7 @@ namespace Level
             }
         }
 
-        private System.Collections.IEnumerator FadeOutTile(Vector3Int tilePosition)
+        private IEnumerator FadeOutTile(Vector3Int tilePosition)
         {
             float duration = 2.0f; // 淡入持续时间
             float elapsed = 0;
@@ -123,61 +114,4 @@ namespace Level
             wallTilemap.SetColor(tilePosition, new Color(1, 1, 1, 0));
         }
     }
-
-    //显示电波
-    //public void Show()
-    //{
-    //    //animator.SetTrigger("Show");
-
-    //    //throw new System.NotImplementedException();
-    //}
-
-    ////[Range(0, 360)] public float Angle = 90f;
-    ////[Range(0, 100)] public float distance = 25f;
-    ////public float rotatePerSecond = 90f;
-
-    //private void CheckCollision()
-    //{
-    //    Vector2 face = transform.forward.normalized;
-    //    var colliders = Physics.OverlapSphere(transform.position, 1f, LayerMask.GetMask("wall"));
-    //    var walls = new List<GameObject>();
-    //    foreach (var col in colliders)
-    //    {
-    //        Vector2 pos = (col.transform.position - transform.position).normalized; //获取碰撞到的墙壁的位置
-    //        var angleBetween = Vector2.Angle(face, pos);
-    //        if (angleBetween < Angle / 2)
-    //        {
-    //            if (Physics.Raycast(transform.position, pos, out var hit, 1f))
-    //            {
-    //                Debug.DrawRay(transform.position, transform.forward.normalized * distance, Color.green);
-    //                if (hit.collider == col)
-    //                {
-    //                    walls.Add(col.gameObject);
-    //                }
-    //            }
-    //        }
-    //    }
-
-    //    // if (LookAround(Quaternion.identity, Color.green))
-    //    // {
-    //    // }
-
-
-    //    //cast put walls list to delight
-    //    // EventCenter.Broadcast(new WaveHitWallEvent
-    //    // {
-    //    //     hittedWalls = walls
-    //    // });
-    //    //throw new System.NotImplementedException();
-    //}
-
-    //private bool LookAround(Quaternion eulerAnger, Color DebugColor)
-    //{
-    //    Debug.DrawRay(transform.position, eulerAnger * transform.forward.normalized * distance, DebugColor);
-
-    //    var hit = Physics2D.Raycast(transform.position, eulerAnger * transform.forward, distance);
-    //    if (!hit) return false;
-    //    Debug.Log(hit.collider.gameObject.name);
-    //    return true;
-    //}
 }
---
+++ Assets/Scripts/Level/WallVisibilityManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Assets.Scripts.Level
+{
+    public class WallVisibilityManager : MonoBehaviour
+    {
+        private Tilemap wallTilemap;
+
+        private void Awake()
+        {
+            // 获取墙的Tilemap
+            wallTilemap = GameObject.FindGameObjectWithTag("Map").GetComponent<Tilemap>();
+
+            // 获取地图边界
+            wallTilemap.CompressBounds();
+            var bounds = wallTilemap.cellBounds;
+
+            // 遍历所有瓦片
+            for (var x = bounds.min.x; x < bounds.max.x; x++)
+            {
+                for (var y = bounds.min.y; y < bounds.max.y; y++)
+                {
+                    var tilePosition = new Vector3Int(x, y, 0);
+
+                    if (!wallTilemap.HasTile(tilePosition)) continue;
+                    // 移除瓦片的颜色锁定
+                    wallTilemap.SetTileFlags(tilePosition, TileFlags.None);
+                    // 设置瓦片为完全透明
+                    wallTilemap.SetColor(tilePosition, new Color(1, 1, 1, 0));
+                }
+            }
+        }
+    }
+}
